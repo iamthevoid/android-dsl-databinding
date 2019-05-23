9abffeea16880a39fb17cd56cf25cac30e0d813c
iam <alesetar@gmail.com>
2019-05-23 15:32:28 +0300

    updated web view (fix)

--- components/src/main/java/thevoid/iam/components/ext/WebViewExtensions.kt
+++ components/src/main/java/thevoid/iam/components/ext/WebViewExtensions.kt
@@ -4,5 +4,5 @@ import android.webkit.WebView
 import io.reactivex.Flowable
 
 
-fun <T : CharSequence> WebView.setHtml(htmlFlowable: Flowable<T>, baseUrl : String = "", mimeType : String = "text/html", encoding : String = "UTF-8", historyUrl : String? = null) =
-    addSetter(htmlFlowable) { loadDataWithBaseURL(baseUrl, it.toString(), mimeType, encoding, null) }
+fun <T : CharSequence> WebView.setHtml(htmlFlowable: Flowable<T>, baseUrl : String = "", mimeType : String = "text/html", encoding : String = "utf-8", historyUrl : String? = null) =
+    addSetter(htmlFlowable) { loadDataWithBaseURL(baseUrl, it.toString(), mimeType, encoding, historyUrl) }
