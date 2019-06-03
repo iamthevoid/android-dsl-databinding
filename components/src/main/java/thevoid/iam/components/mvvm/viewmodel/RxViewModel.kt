45dfb2b2ea5b4452e063f72182f764e027f125a4
iam <alesetar@gmail.com>
2019-06-03 14:27:41 +0300

    resubscribe on active

--- components/src/main/java/thevoid/iam/components/mvvm/viewmodel/RxViewModel.kt
+++ components/src/main/java/thevoid/iam/components/mvvm/viewmodel/RxViewModel.kt
@@ -7,16 +7,23 @@ import io.reactivex.disposables.Disposable
 
 open class RxViewModel : LifecycleTrackingViewModel() {
 
+    private val list by lazy { mutableListOf<Disposable>() }
+
     private val disposable by lazy { CompositeDisposable() }
 
     protected fun unsubscribeOnInactive(disposable: Disposable, vararg disposables: Disposable) {
         this.disposable.add(disposable)
-        if (disposables.isNotEmpty())
+        this.list.add(disposable)
+        if (disposables.isNotEmpty()) {
             this.disposable.addAll(*disposables)
+            disposables.forEach { list.add(it) }
+        }
     }
 
+    @CallSuper
     override fun onActive() {
-
+        if (list.isNotEmpty())
+            list.forEach { disposable.add(it) }
     }
 
     @CallSuper
