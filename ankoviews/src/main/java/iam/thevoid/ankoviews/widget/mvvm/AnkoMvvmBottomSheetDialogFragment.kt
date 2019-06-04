0e505106477ab5451f13e1890eda171bd6fac3c2
iam <alesetar@gmail.com>
2019-06-04 17:15:54 +0300

    anko bottom sheet dialog (0.6.16)

---
+++ ankoviews/src/main/java/iam/thevoid/ankoviews/widget/mvvm/AnkoMvvmBottomSheetDialogFragment.kt
@@ -0,0 +1,13 @@
+package iam.thevoid.ankoviews.widget.mvvm
+
+import android.view.View
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import thevoid.iam.components.mvvm.view.MvvmBottomSheetDialogFragment
+import thevoid.iam.components.mvvm.view.MvvmDialogFragment
+
+abstract class AnkoMvvmBottomSheetDialogFragment : MvvmBottomSheetDialogFragment(), AnkoComponent<AnkoMvvmBottomSheetDialogFragment> {
+
+    override fun provideContentView(): View? = context?.let { createView(AnkoContext.create(it, this, false)) }
+
+}
