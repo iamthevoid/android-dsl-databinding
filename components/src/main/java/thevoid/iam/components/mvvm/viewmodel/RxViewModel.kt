8ca2dbc51c3b6e251099fcb1f741de49ad8a1ee2
iam <alesetar@gmail.com>
2019-07-03 13:36:16 +0300

    updated disposables (0.7.27)

--- components/src/main/java/thevoid/iam/components/mvvm/viewmodel/RxViewModel.kt
+++ components/src/main/java/thevoid/iam/components/mvvm/viewmodel/RxViewModel.kt
@@ -10,15 +10,23 @@ open class RxViewModel : LifecycleTrackingViewModel() {
     private val onInactiveDisposables by lazy { CompositeDisposable() }
     private val onClearDisposables by lazy { CompositeDisposable() }
 
-    protected fun toDispose(disposable: Disposable?, vararg disposables: Disposable?) {
-        disposable?.also { onInactiveDisposables.add(it) }
-        disposables.forEach { it?.also { disp -> onInactiveDisposables.add(disp) } }
-    }
+    protected fun toDispose(disposable: Disposable): Disposable =
+        disposable.also { onInactiveDisposables.add(it) }
 
-    protected fun toDisposeOnCleared(disposable: Disposable?, vararg disposables: Disposable?) {
-        disposable?.also { onClearDisposables.add(it) }
-        disposables.forEach { it?.also { disp -> onClearDisposables.add(disp) } }
-    }
+    protected fun toDispose(disposable: Disposable?, vararg disposables: Disposable?): Disposable =
+        CompositeDisposable().apply {
+            disposable?.also { toDispose(it) }?.also { add(it) }
+            disposables.mapNotNull { toDispose(it) }.onEach { add(it) }
+        }
+
+    protected fun toDisposeOnCleared(disposable: Disposable): Disposable =
+        disposable.also { onClearDisposables.add(it) }
+
+    protected fun toDisposeOnCleared(disposable: Disposable?, vararg disposables: Disposable?): Disposable =
+        CompositeDisposable().apply {
+            disposable?.also { toDisposeOnCleared(it) }?.also { add(it) }
+            disposables.mapNotNull { toDisposeOnCleared(it) }.onEach { add(it) }
+        }
 
     override fun onActive() {
     }
