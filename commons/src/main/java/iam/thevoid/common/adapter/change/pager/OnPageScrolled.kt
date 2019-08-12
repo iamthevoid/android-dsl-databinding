2538631157b531fa90519d09b9987e273e595398
iam <alesetar@gmail.com>
2019-08-12 12:18:29 +0300

    pager two way binding (0.9.5)

---
+++ commons/src/main/java/iam/thevoid/common/adapter/change/pager/OnPageScrolled.kt
@@ -0,0 +1,3 @@
+package iam.thevoid.common.adapter.change.pager
+
+data class OnPageScrolled(val position: Int, val positionOffset: Float, val positionOffsetPixels: Int)
--- rx/src/main/java/thevoid/iam/rx/widget/ext/ViewPagerExtensions.kt
+++ rx/src/main/java/thevoid/iam/rx/widget/ext/ViewPagerExtensions.kt
@@ -6,19 +6,30 @@ import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import iam.thevoid.ae.asAppCompatActivity
+import iam.thevoid.common.adapter.adapters.OnPageChangeListenerAdapter
+import iam.thevoid.common.adapter.change.pager.OnPageScrolled
+import iam.thevoid.common.adapter.delegate.OnPageChangeDelegate
 import iam.thevoid.rxe.combine
 import io.reactivex.Flowable
+import thevoid.iam.rx.R
 import thevoid.iam.rx.adapter.ItemBindings
 import thevoid.iam.rx.adapter.RxPagerAdapter
 import thevoid.iam.rx.adapter.SimpleFragmentPagerAdapter
+import thevoid.iam.rx.rxdata.fields.RxField
 import thevoid.iam.rx.rxdata.fields.RxInt
+import thevoid.iam.rx.rxdata.fields.RxItem
 import thevoid.iam.rx.rxdata.fields.RxList
 
-fun ViewPager.setCurrentPage(page: Flowable<Int>, smoothScroll: Boolean = true) =
-    addSetter(page) { setCurrentItem(it, smoothScroll) }
-
 fun ViewPager.setCurrentPage(page: RxInt, smoothScroll: Boolean = true) =
-    addSetter(page.observe()) { setCurrentItem(it, smoothScroll) }
+    setCurrentPage(page.observe(), smoothScroll)
+
+fun ViewPager.setCurrentPage(page: Flowable<Int>, smoothScroll: Boolean = true) =
+    addSetter(page) {
+        val onPageChangeListener = onPageChangeListener
+        removeOnPageChangeListener(onPageChangeListener)
+        setCurrentItem(it, smoothScroll)
+        addOnPageChangeListener(onPageChangeListener)
+    }
 
 // View Pager Adapter
 
@@ -45,10 +56,10 @@ fun <T : Any> ViewPager.setItems(
 ) = addSetter(combine(itemsFlowable, titlesFlowable)) { setItems(it.first, itemBindings, it.second) }
 
 fun <T : Any> ViewPager.setItems(
-    items : List<T>,
+    items: List<T>,
     itemBindings: ItemBindings,
-    titles : List<String> = emptyList(),
-    position : Int = PagerAdapter.POSITION_UNCHANGED
+    titles: List<String> = emptyList(),
+    position: Int = PagerAdapter.POSITION_UNCHANGED
 ) {
     (adapter as? RxPagerAdapter<T>)?.apply {
         setTitledItems(RxPagerAdapter.fromTitlesAndItems(items, titles))
@@ -89,3 +100,69 @@ fun ViewPager.setTitledFactories(
     adapter = (adapter as? SimpleFragmentPagerAdapter)?.apply { setTitledFactories(fragments) }
         ?: SimpleFragmentPagerAdapter(fm, fragments, behavior)
 }
+
+// On Page Change Listener
+
+private val ViewPager.onPageChangeListener: OnPageChangeDelegate
+    get() = ((getTag(R.id.onPageChangeListener) as? OnPageChangeDelegate)
+        ?: OnPageChangeDelegate().also {
+            setTag(R.id.onTabSelectListener, it)
+            addOnPageChangeListener(it)
+        })
+
+
+fun ViewPager.onPageSelect(rxPage: RxInt) =
+    onPageSelect(rxPage) { it }
+
+fun <T : Any> ViewPager.onPageSelect(rxField: RxField<T>, mapper: ((Int) -> T?)) =
+    addGetter({
+        onPageChangeListener.addOnPageSelectedCallback(object : OnPageChangeListenerAdapter() {
+            override fun onPageSelected(position: Int) {
+                it.invoke(mapper(position))
+            }
+        })
+    }, rxField)
+
+fun <T : Any> ViewPager.onPageSelect(rxItem: RxItem<T>, mapper: ((Int) -> T)) =
+    addGetter({
+        onPageChangeListener.addOnPageSelectedCallback(object : OnPageChangeListenerAdapter() {
+            override fun onPageSelected(position: Int) {
+                it.invoke(mapper(position))
+            }
+        })
+    }, rxItem)
+
+
+fun ViewPager.onPageScrollStateChanged(rxInt: RxInt) =
+    onPageScrollStateChanged(rxInt) { it }
+
+fun <T : Any> ViewPager.onPageScrollStateChanged(rxField: RxField<T>, mapper: ((Int) -> T)) =
+    addGetter({
+        onPageChangeListener.addOnPageScrollStateChanged(object : OnPageChangeListenerAdapter() {
+            override fun onPageScrollStateChanged(state: Int) {
+                it.invoke(mapper(state))
+            }
+        })
+    }, rxField)
+
+fun <T : Any> ViewPager.onPageScrollStateChanged(rxItem: RxItem<T>, mapper: ((Int) -> T)) =
+    addGetter({
+        onPageChangeListener.addOnPageScrollStateChanged(object : OnPageChangeListenerAdapter() {
+            override fun onPageScrollStateChanged(state: Int) {
+                it.invoke(mapper(state))
+            }
+        })
+    }, rxItem)
+
+fun ViewPager.onPageScrolled(rxField: RxField<OnPageScrolled>) =
+    onPageScrolled(rxField) { it }
+
+fun <T : Any> ViewPager.onPageScrolled(rxField: RxField<T>, mapper: ((OnPageScrolled) -> T)) =
+    addGetter({
+        onPageChangeListener.addOnPageScrolledCallback(object : OnPageChangeListenerAdapter() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                it.invoke(mapper(OnPageScrolled(position, positionOffset, positionOffsetPixels)))
+            }
+        })
+    }, rxField)
+
