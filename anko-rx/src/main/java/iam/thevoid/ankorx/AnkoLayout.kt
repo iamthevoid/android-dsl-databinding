d9d5028fb097769ea2596f95c8c2bd4fdcc6b00c
iamthevoid <a.tarabrin@butik.ru>
2019-09-27 11:46:27 +0300

    update

--- anko-rx/src/main/java/iam/thevoid/ankorx/AnkoLayout.kt
+++ anko-rx/src/main/java/iam/thevoid/ankorx/AnkoLayout.kt
@@ -6,6 +6,7 @@ import org.jetbrains.anko.AnkoComponent
 import org.jetbrains.anko.AnkoContext
 import thevoid.iam.rx.adapter.Layout
 
+
 abstract class AnkoLayout<T>(parent: ViewGroup) : Layout<T>(parent), AnkoComponent<AnkoLayout<T>> {
     override fun createView(parent: ViewGroup): View =
         createView(AnkoContext.create(parent.context, this, false))
