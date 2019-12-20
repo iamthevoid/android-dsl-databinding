f1264cfbdcfafd1af2f6fcaf2658a11a8c004822
iamthevoid <a.tarabrin@butik.ru>
2019-12-20 11:24:52 +0300

    added two loadings refreshing

--- rx/src/main/java/thevoid/iam/rx/widget/ext/SwipeRefreshLayoutExtensions.kt
+++ rx/src/main/java/thevoid/iam/rx/widget/ext/SwipeRefreshLayoutExtensions.kt
@@ -1,6 +1,7 @@
 package thevoid.iam.rx.widget.ext
 
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import iam.thevoid.rxe.combine
 import io.reactivex.Flowable
 import thevoid.iam.rx.rxdata.RxLoading
 import thevoid.iam.rx.rxdata.fields.RxBoolean
@@ -8,6 +9,11 @@ import thevoid.iam.rx.rxdata.fields.RxBoolean
 
 fun SwipeRefreshLayout.setRefreshing(loading : RxLoading) =
         addSetter(loading.observe()) { isRefreshing = it }
+
+fun SwipeRefreshLayout.setRefreshing(loading1: RxLoading, loading2: RxLoading) =
+        addSetter(combine(loading1.observe(), loading2.observe())) { (f, s) ->
+                isRefreshing = f || s
+        }
 
 fun SwipeRefreshLayout.setRefreshing(loading : RxBoolean) =
         addSetter(loading.observe()) { isRefreshing = it }
