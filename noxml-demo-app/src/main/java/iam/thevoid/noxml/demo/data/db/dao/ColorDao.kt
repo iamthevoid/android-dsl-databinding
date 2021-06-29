edcef6addba41282dd6c6b7e5f328ebab1dae928
iam <alesetar@gmail.com>
2021-06-29 10:09:56 +0300

    Removed publishing to jfrog, updated libraries versions

--- noxml-demo-app/src/main/java/iam/thevoid/noxml/demo/data/db/dao/ColorDao.kt
+++ noxml-demo-app/src/main/java/iam/thevoid/noxml/demo/data/db/dao/ColorDao.kt
@@ -15,13 +15,6 @@ abstract class ColorDao {
     @Query("SELECT * FROM Color WHERE id = 1 LIMIT 1")
     abstract fun dbColorFlow() : Flow<List<Color>>
 
-    @Query("SELECT * FROM Color WHERE id = 1 LIMIT 1")
-    abstract suspend fun dbColor() : List<Color>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun update(color: Color)
-
     fun colorFlow() = dbColorFlow().map { Optional.of(it.firstOrNull()) }
 
-    suspend fun color() = dbColor().let { Optional.of(it.firstOrNull()) }
 }
